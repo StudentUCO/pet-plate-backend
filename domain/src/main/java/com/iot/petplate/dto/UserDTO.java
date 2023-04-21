a3ce5f1f9046cf1e939ae6d4c81d44583fd26a07
Brayan Sepúlveda <brayansepulveda@saimyr.com.co>
2023-04-20 20:58:24 -0500

    refactor a UserDTO.java para que no salga error al deserializar el objeto json

--- domain/src/main/java/com/iot/petplate/dto/UserDTO.java
+++ domain/src/main/java/com/iot/petplate/dto/UserDTO.java
@@ -2,10 +2,14 @@ package com.iot.petplate.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
-public class UserDTO {
+@NoArgsConstructor
+public class UserDTO implements Serializable {
     private Integer id;
     private String fullName;
     private String phoneNumber;
